3463883e31b2fa6f32b15e75e0822e5aada4d9f4
haresh2349 <hareshcsolanki07@gmail.com>
2022-08-25 00:52:25 +0530

    updated

--- src/Components/MainRoutes.jsx
+++ src/Components/MainRoutes.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import BigTeam from '../why-track/Forbigteams/BigTeam';
-import  Freelancers from '../why-track/Forfreelancers/Freelancer'
-import SmallTeam from '../why-track/Forsmallteam/SmallTeam';
+import BigTeam from "../why-track/Forbigteams/BigTeam";
+import Freelancers from "../why-track/Forfreelancers/Freelancer";
+import SmallTeam from "../why-track/Forsmallteam/SmallTeam";
 const MainRoutes = () => {
   return (
     <Routes>
-      {/* <Navbar/> */}
-<<<<<<< HEAD
-      <Route>{/* <Route path='/' element={<Home/>}/> */}</Route>
-=======
-      <Routes>
-        <Route path="/track/freelance-time-tracking" element={<Freelancers />} />        
-        <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
-        <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
-      </Routes>
->>>>>>> 085bdaacfdfb382f5e3f3e8318b693db99afa7e6
-      {/* <Footer/> */}
+      <Route path="/track/freelance-time-tracking" element={<Freelancers />} />
+      <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
+      <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
     </Routes>
   );
 };
