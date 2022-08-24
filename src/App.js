cc24f486be6eed7577fe5b9a4a83de1ee9420f9a
haresh2349 <hareshcsolanki07@gmail.com>
2022-08-25 00:37:02 +0530

    mainroutes updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
 import Pricing from "./Pages/Pricing";
 import TogglTrackPage from "./Pages/TogglTrackPage/TogglTrackPage";
-import MainRoutes from './Components/MainRoutes';
+import MainRoutes from "./Components/MainRoutes";
 
 function App() {
   return (
     <Box className="App">
-      <MainRoutes/>
-      <Pricing />
-      <TogglTrackPage/>
-      
+      <MainRoutes />
     </Box>
-
-
-
-
-
-  )
+  );
 }
 
 export default App;
--- src/Components/MainRoutes.jsx
+++ src/Components/MainRoutes.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import BigTeam from '../why-track/Forbigteams/BigTeam';
-import  Freelancers from '../why-track/Forfreelancers/Freelancer'
-import SmallTeam from '../why-track/Forsmallteam/SmallTeam';
+import Pricing from "../Pages/Pricing";
+import Signup from "../Pages/Signup";
+import TogglTrackPage from "../Pages/TogglTrackPage/TogglTrackPage";
+import BigTeam from "../why-track/Forbigteams/BigTeam";
+import Freelancers from "../why-track/Forfreelancers/Freelancer";
+import SmallTeam from "../why-track/Forsmallteam/SmallTeam";
 const MainRoutes = () => {
   return (
     <Routes>
-      {/* <Navbar/> */}
-<<<<<<< HEAD
-      <Route>{/* <Route path='/' element={<Home/>}/> */}</Route>
-=======
-      <Routes>
-        <Route path="/track/freelance-time-tracking" element={<Freelancers />} />        
-        <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
-        <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
-      </Routes>
->>>>>>> 085bdaacfdfb382f5e3f3e8318b693db99afa7e6
-      {/* <Footer/> */}
+      <Route path="/track/signup" element={<Signup />} />
+      <Route path="/track/freelance-time-tracking" element={<Freelancers />} />
+      <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
+      <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
+      <Route path="/track/pricing" element={<Pricing />} />
+      <Route path="/track/timer" element={<TogglTrackPage />} />
     </Routes>
   );
 };
