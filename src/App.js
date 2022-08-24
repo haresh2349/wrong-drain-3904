f5a82c1670997dc33d301369eb58b8def8627d18
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-08-24 17:00:37 +0530

    completed day-2 work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
-import Signup from "./Pages/Signup";
-// import Pricing from "./Pages/Pricing";
+import Pricing from "./Pages/Pricing";
 
 function App() {
   return (
     <Box className="App">
-      {/* <Pricing /> */}
-      <Signup/>
+      <Pricing />
     </Box>
   );
 }
