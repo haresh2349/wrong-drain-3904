16ea038fbd428dc6b827c96330c398eba0de7698
Manish Singh Chouhan <100896129+manishchouhan2396@users.noreply.github.com>
2022-08-26 09:37:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
-import Pricing from "./Pages/Pricing";
-import TogglTrackPage from "./Pages/TogglTrackPage/TogglTrackPage";
+
+
 import MainRoutes from "./Components/MainRoutes";
-import Signup from "./Pages/Signup";
+
 
 function App() {
   return (
     <Box className="App">
-      {/* <MainRoutes /> */}
-      <Signup/>
+       <MainRoutes /> 
     </Box>
   );
 }
