a5efd26caac85044f66ed56e3284dcb937fdd91e
Manish Singh Chouhan <100896129+manishchouhan2396@users.noreply.github.com>
2022-08-26 09:35:57 +0530

    Update MainRoutes.jsx

--- src/Components/MainRoutes.jsx
+++ src/Components/MainRoutes.jsx
@@ -3,16 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import BigTeam from '../why-track/Forbigteams/BigTeam';
 import  Freelancers from '../why-track/Forfreelancers/Freelancer'
 import SmallTeam from '../why-track/Forsmallteam/SmallTeam';
+import Pricing from "../Pages/Pricing";
+import Signup from "../Pages/Signup";
+import TogglTrackPage from "../Pages/TogglTrackPage/TogglTrackPage";
 const MainRoutes = () => {
   return (
     <Routes>
       {/* <Navbar/> */}
-      {/* <Route><Route path='/' element={<Home/>}/></Route> */}
 
       <Routes>
-        <Route path="/track/freelance-time-tracking" element={<Freelancers />} />        
-        <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
-        <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
+        
+            <Route path="/track/signup" element={<Signup />} />
+          <Route path="/track/freelance-time-tracking" element={<Freelancers />} />
+          <Route path="/track/time-tracking-small-teams" element={<SmallTeam />} />
+         <Route path="/track/time-tracking-large-teams" element={<BigTeam />} />
+         <Route path="/track/pricing" element={<Pricing />} />
+        <Route path="/track/timer" element={<TogglTrackPage />} />
+       
       </Routes>
 
       {/* <Footer/> */}
