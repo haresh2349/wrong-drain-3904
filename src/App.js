ac1410034614f1b6c8a2044636897c89ff9c5ec4
Bhavesh2302 <jainbhavesh888@gmail.com>
2022-08-27 00:38:45 +0530

    added some more css and functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 // import { Box } from "@chakra-ui/react";
 import Pricing from "./Pages/Pricing";
 import TogglTrackPage from "./Pages/TogglTrackPage/TogglTrackPage";
-import MainRoutes from "./Components/MainRoutes";
+
 import { Box } from "@chakra-ui/react";
 import { MainRoutes } from "./Components/MainRoutes";
 function App() {
